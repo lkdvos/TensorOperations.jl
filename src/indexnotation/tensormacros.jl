5f500d10d4e55fb371e9269f6ae6bef66f65db56
lkdvos <lukas.devos@ugent.be>
2023-07-13 14:57:15 +0200

    Move @cutensor iimplementation

--- src/indexnotation/tensormacros.jl
+++ src/indexnotation/tensormacros.jl
@@ -261,3 +261,24 @@ function _nconmacro(tensors, indices, kwargs=nothing)
     end
     return esc(ex)
 end
+
+"""
+    @cutensor tensor_expr
+
+Use the GPU to perform all tensor operations, through the use of the cuTENSOR library.
+This will transfer all arrays to the GPU before performing the requested operations. If the
+output is an existing host array, the result will be transferred back. If a new array is
+created (i.e. using `:=`), it will remain on the GPU device and it is up to the user to
+transfer it back.
+
+!!! note
+    This macro requires the cuTENSOR library to be installed and loaded. This can be
+    achieved by running `using cuTENSOR` or `import cuTENSOR` before using the macro.
+"""
+macro cutensor(ex::Expr)
+    haskey(Base.loaded_modules, Base.identify_package("cuTENSOR")) ||
+        throw(ArgumentError("cuTENSOR not loaded: add `using cuTENSOR` or `import cuTENSOR` before using `@cutensor`"))
+    parser = TensorParser()
+    push!(parser.postprocessors, ex -> insertbackend(ex, :cuda))
+    return esc(parser(ex))
+end
